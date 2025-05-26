5813ea87cbba42f57284d7913a1113dde81e661c
achinim2004 <achinthailushaugc@gmail.com>
2025-05-26 23:37:24 +0530

    Initial Commit

---
+++ Exercise  01/a.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Subjects \t Marks \n");
+    printf("--------------------------\n");
+    printf("Mathematics \t 90 \n");
+    printf("English \t 85 \n");
+     printf("Science \t 82 \n");
+
+   
+    return 0;
+
+
+
+
+}
